ea80cae5a0fea083f232f2a1f887373086618bfa
Michael McLoughlin <mmcloughlin@gmail.com>
2017-10-02 16:35:21 -0700

    init

---
+++ scanner.py
@@ -0,0 +1,71 @@
+import sys
+import json
+import logging
+import random
+
+from sslyze.concurrent_scanner import ConcurrentScanner
+from sslyze.server_connectivity import ServerConnectivityInfo
+from sslyze.plugins.openssl_cipher_suites_plugin import *
+
+
+def split_host_port(addr):
+    ip, port_str = addr.split(':')
+    return ip, int(port_str)
+
+
+def scan_commands():
+    return [
+            Tlsv10ScanCommand(),
+            ]
+
+
+def scan_cipher_lists(addresses):
+    # prepare scanner
+    s = ConcurrentScanner()
+    for addr in addresses:
+        ip, port = split_host_port(addr)
+        server_info = ServerConnectivityInfo(hostname=addr, ip_address=ip, port=port)
+        server_info.test_connectivity_to_server()
+        for cmd in scan_commands():
+            s.queue_scan_command(server_info, cmd)
+
+    # execute
+    for result in s.get_results(): 
+        for cipher in result.accepted_cipher_list:
+            print result.server_info.hostname, cipher.name, cipher.ssl_version
+
+
+def read_or_addresses(f):
+    """
+    Extract or_addresses from output of
+    https://onionoo.torproject.org/details.
+    """
+    data = json.load(f)
+    relays = data['relays']
+
+    addresses = []
+    for relay in relays:
+        if not relay['running']:
+            continue
+        addresses.append(relay['or_addresses'][0])
+
+    return addresses
+
+
+def main(args):
+    logging.basicConfig(level=logging.DEBUG)
+
+    filename = args[0]
+    sample_size = int(args[1])
+
+    with open(filename) as f:
+        addresses = read_or_addresses(f)
+
+    sample = random.sample(addresses, sample_size)
+    logging.info('sampled %d of %d running relays', sample_size, len(addresses))
+
+    scan_cipher_lists(sample)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
